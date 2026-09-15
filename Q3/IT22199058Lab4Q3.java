8389a80d56ab1fe93f8b4dfd634dca4fcba3ab90
Kavindu Dayan <it22199058@my.sliit.lk>
2026-09-15 14:00:33 +0530

    Add files via upload

---
+++ Q3/IT22199058Lab4Q3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IT22199058Lab4Q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = input.nextDouble();
+
+        String result = (number > 0) ? "Positive" : (number < 0) ? "Negative" : "Zero";
+
+        System.out.println("The number is: " + result);
+
+        input.close();
+    }
+}
